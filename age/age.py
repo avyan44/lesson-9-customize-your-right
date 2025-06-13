6f351bcb09068af1ac228662a5fb24ab88ee98fb
avyan44 <sumianubha@gmail.com>
2025-06-13 21:36:04 +0100

    age

--- age/age.py
+++ age/age.py
@@ -1,12 +1,10 @@
-def check_year_10_eligibility():
-    name = input("Enter your name: ")
-    age = int(input("Enter your age: "))
 
-    if age < 10:
-        print(f"Sorry {name}, you can't join Year 10.")
-    else:
-        print(f"Welcome {name}, you're eligible to join Year 10!")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
 
-# Run the function
-check_year_10_eligibility()
+if age < 10:
+    print("Sorry", name + ", you can't join Year 10.")
+else:
+    print("Welcome", name + ", you're eligible to join Year 10!")
+
 
